e3ac80193fd67d8193515622c37fae12226b3629
Pavel <lazycrafter74@gmail.com>
2019-12-17 19:10:21 +0300

    Initial

--- lstm.py
+++ lstm.py
@@ -1,46 +1,44 @@
-""" This module prepares midi file data and feeds it to the neural
-    network for training """
 import glob
 import pickle
 import numpy
 from music21 import converter, instrument, note, chord
+from keras.utils import plot_model
+from keras.engine import InputLayer
 from keras.models import Sequential
-from keras.layers import Dense
-from keras.layers import Dropout
-from keras.layers import LSTM
-from keras.layers import Activation
-from keras.layers import BatchNormalization as BatchNorm
+from keras.layers import Dense, GRU, Dropout, Activation, BatchNormalization
 from keras.utils import np_utils
 from keras.callbacks import ModelCheckpoint
+from tensorflow import keras
+
 
 def train_network():
-    """ Train a Neural Network to generate music """
     notes = get_notes()
 
-    # get amount of pitch names
     n_vocab = len(set(notes))
 
     network_input, network_output = prepare_sequences(notes, n_vocab)
 
     model = create_network(network_input, n_vocab)
 
+    plot_model(model, to_file='model.png')
+
     train(model, network_input, network_output)
 
+
 def get_notes():
-    """ Get all the notes and chords from the midi files in the ./midi_songs directory """
     notes = []
 
-    for file in glob.glob("midi_songs/*.mid"):
+    for file in glob.glob("midi_songs_new/*.mid"):
         midi = converter.parse(file)
 
         print("Parsing %s" % file)
 
         notes_to_parse = None
 
-        try: # file has instrument parts
+        try:
             s2 = instrument.partitionByInstrument(midi)
             notes_to_parse = s2.parts[0].recurse() 
-        except: # file has notes in a flat structure
+        except:
             notes_to_parse = midi.flat.notes
 
         for element in notes_to_parse:
@@ -54,20 +52,17 @@ def get_notes():
 
     return notes
 
+
 def prepare_sequences(notes, n_vocab):
-    """ Prepare the sequences used by the Neural Network """
     sequence_length = 100
 
-    # get all pitch names
     pitchnames = sorted(set(item for item in notes))
 
-     # create a dictionary to map pitches to integers
     note_to_int = dict((note, number) for number, note in enumerate(pitchnames))
 
     network_input = []
     network_output = []
 
-    # create input sequences and the corresponding outputs
     for i in range(0, len(notes) - sequence_length, 1):
         sequence_in = notes[i:i + sequence_length]
         sequence_out = notes[i + sequence_length]
@@ -76,51 +71,47 @@ def prepare_sequences(notes, n_vocab):
 
     n_patterns = len(network_input)
 
-    # reshape the input into a format compatible with LSTM layers
     network_input = numpy.reshape(network_input, (n_patterns, sequence_length, 1))
-    # normalize input
     network_input = network_input / float(n_vocab)
 
     network_output = np_utils.to_categorical(network_output)
 
-    return (network_input, network_output)
+    return network_input, network_output
+
 
 def create_network(network_input, n_vocab):
-    """ create the structure of the neural network """
     model = Sequential()
-    model.add(LSTM(
-        512,
-        input_shape=(network_input.shape[1], network_input.shape[2]),
+    model.add(InputLayer(input_shape=(network_input.shape[1], network_input.shape[2]),))
+    model.add(GRU(
+        128,
         recurrent_dropout=0.3,
         return_sequences=True
     ))
-    model.add(LSTM(512, return_sequences=True, recurrent_dropout=0.3,))
-    model.add(LSTM(512))
-    model.add(BatchNorm())
-    model.add(Dropout(0.3))
-    model.add(Dense(256))
+    model.add(GRU(128, return_sequences=True, recurrent_dropout=0.3,))
+    model.add(GRU(128))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
+    model.add(Dense(64))
     model.add(Activation('relu'))
-    model.add(BatchNorm())
-    model.add(Dropout(0.3))
+    model.add(BatchNormalization())
+    model.add(Dropout(0.2))
     model.add(Dense(n_vocab))
     model.add(Activation('softmax'))
-    model.compile(loss='categorical_crossentropy', optimizer='rmsprop')
+    model.compile(loss='categorical_crossentropy', optimizer='rmsprop',
+                  metrics=[keras.metrics.CategoricalAccuracy()])
+
+    model.load_weights('weight_new/weights-improvement-107-0.5131-bigger.hdf5')
 
     return model
 
-def train(model, network_input, network_output):
-    """ train the neural network """
-    filepath = "weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
-    checkpoint = ModelCheckpoint(
-        filepath,
-        monitor='loss',
-        verbose=0,
-        save_best_only=True,
-        mode='min'
-    )
-    callbacks_list = [checkpoint]
 
-    model.fit(network_input, network_output, epochs=200, batch_size=128, callbacks=callbacks_list)
+def train(model, network_input, network_output):
+    filepath = "weight_new/weights-improvement-{epoch:02d}-{loss:.4f}-bigger.hdf5"
+
+    checkpoint = ModelCheckpoint(filepath, monitor='loss', verbose=0, save_best_only=True, mode='min')
+
+    model.fit(network_input, network_output, epochs=200, batch_size=256, callbacks=[checkpoint])
+
 
 if __name__ == '__main__':
     train_network()
